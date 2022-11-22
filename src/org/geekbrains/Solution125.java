5b941b6f7d3218882d627df1f7f9fa00e769ceb7
foralcome <foralcome@mail.ru>
2022-11-22 21:19:18 +0700

    125

---
+++ src/org/geekbrains/Solution125.java
@@ -0,0 +1,24 @@
+package org.geekbrains;
+
+public class Solution125 {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    static public boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i <= j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
