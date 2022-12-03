ae9abc2bbb27a94cf3bb2e628a30b3d6d451daf9
foralcome <foralcome@mail.ru>
2022-12-03 13:40:38 +0700

    257

--- src/org/geekbrains/Solution257.java
+++ src/org/geekbrains/Solution257.java
@@ -1,22 +1,54 @@
 package org.geekbrains;
 
-import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class Solution257 {
 
     public static void main(String[] args) {
-        List<Integer> data = new ArrayList<>();
+        //root =  0 1 2  3   4
+        int[] data = {1, 2, 3, 0, 5};
+        TreeNode root = new TreeNode();
+        root = load(0, data);
+
+        List<String> l = new ArrayList<>();
+        l = binaryTreePaths(root);
+        printListPath(l);
     }
 
-    public List<String> binaryTreePaths(TreeNode root) {
+    public static TreeNode load(int index, int[] data) {
+        if (index > data.length) return null;
+
+        TreeNode node = new TreeNode(data[index]);
+
+        int indexLeft = index * 2 + 1;
+        if (indexLeft < data.length) {
+            node.left = null;
+            if (data[indexLeft] != 0) {
+                TreeNode nLeft = new TreeNode(data[indexLeft]);
+                node.left = load(indexLeft, data);
+            }
+        }
+        int indexRight = index * 2 + 2;
+        if (indexRight < data.length) {
+            node.right = null;
+            if (data[indexRight] != 0) {
+                TreeNode nRight = new TreeNode(data[indexRight]);
+                node.right = load(indexRight, data);
+            }
+        }
+
+        return node;
+    }
+
+    public static List<String> binaryTreePaths(TreeNode root) {
         List<String> l = new ArrayList<>();
         dfs(root, l, Integer.toString(root.val));
         return l;
     }
 
-    private void dfs(TreeNode node, List<String> l, String path) {
+    private static void dfs(TreeNode node, List<String> l, String path) {
         if (node.left == null && node.right == null)
             l.add(path);
 
@@ -25,6 +57,12 @@ public class Solution257 {
         }
         if (node.right != null) {
             dfs(node.right, l, path + "->" + Integer.toString(node.right.val));
+        }
+    }
+
+    public static void printListPath(List<String> l){
+        for (String s : l){
+            System.out.println(s);
         }
     }
 }
