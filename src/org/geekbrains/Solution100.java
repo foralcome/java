ccb7b7afebf989c938c830e397bb4a5389d7fc90
foralcome <foralcome@mail.ru>
2022-12-07 12:10:57 +0700

    100

--- src/org/geekbrains/Solution100.java
+++ src/org/geekbrains/Solution100.java
@@ -43,9 +43,10 @@ public class Solution100 {
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p != null && q == null) || (p == null && q != null)) return false;
         if (p == null && q == null) return true;
         if (p != null && q != null && p.val != q.val) return false;
-        if ((p != null && q == null) || (p == null && q != null)) return false;
+
 
         boolean checkLeft = isSameTree(p.left, q.left);
         boolean checkRight = isSameTree(p.right, q.right);
